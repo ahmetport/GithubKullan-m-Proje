e61960fa072c445f38f13147d32c437068224bb7
ahmet portakal <ahmettportakal27@gmail.com>
2022-06-04 14:18:06 +0300

    ilk degişiklik

--- src/kişiselKullanım/githubKullanım1.java
+++ src/kişiselKullanım/githubKullanım1.java
@@ -3,7 +3,9 @@ package kişiselKullanım;
 public class githubKullanım1 {
 
     public static void main(String[] args) {
-        System.out.println();("hello world");
+
         System.out.println("That's it");
+        System.out.println("hello world");
+        System.out.println("zalimler için yaşasın cehennem");//degişiklik yapıldıktan sonra uptade edecegiz
     }
 }
