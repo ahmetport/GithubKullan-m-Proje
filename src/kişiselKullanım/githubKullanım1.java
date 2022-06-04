0ef81e9ee23c784fb0ffe40b0982cf7ab4c09ae4
ahmet portakal <ahmettportakal27@gmail.com>
2022-06-04 14:12:35 +0300

    ilkKullanım

--- src/kişiselKullanım/githubKullanım1.java
+++ src/kişiselKullanım/githubKullanım1.java
@@ -3,7 +3,7 @@ package kişiselKullanım;
 public class githubKullanım1 {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println();("hello world");
         System.out.println("That's it");
     }
 }
